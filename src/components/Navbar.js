b51e087cff97ed957829520e9789ab8dca7b33e4
Borgeel <irfancesir93@gmail.com>
2022-10-21 22:41:06 +0200

    navbar done

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,34 +1,56 @@
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
-import NavDropdown from "react-bootstrap/NavDropdown";
+
+import logo from "../assets/images/logo.png";
 
 function CollapsibleExample() {
   return (
     <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
       <Container>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Brand href="/">
+          <img src={logo} alt="logo" width="130rem" />
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
-            <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown>
+            <Nav.Link
+              className="font-secondary fs-10 ms-lg-1 ms-md-3 ms-1"
+              href="/medical-aesthetics"
+            >
+              Medical Aesthetics
+            </Nav.Link>
+            <Nav.Link
+              className="font-secondary fs-10 ms-lg-1 ms-md-3 ms-1"
+              href="/dental-aesthetics"
+            >
+              Dental Aesthetics
+            </Nav.Link>
+            <Nav.Link
+              className="font-secondary fs-10 ms-lg-1 ms-md-3 ms-1"
+              href="/hair-transplantation"
+            >
+              Hair Transplantation
+            </Nav.Link>
+            <Nav.Link
+              className="font-secondary fs-10 ms-lg-1 ms-md-3 ms-1"
+              href="/stem-cell-therapy"
+            >
+              Stem Cell Therapy
+            </Nav.Link>
+            <Nav.Link
+              className="font-secondary fs-10 ms-lg-1 ms-md-3 ms-1"
+              href="/sexual-wellness"
+            >
+              Sexual Wellness
+            </Nav.Link>
           </Nav>
           <Nav>
-            <Nav.Link href="#deets">More deets</Nav.Link>
-            <Nav.Link eventKey={2} href="#memes">
-              Dank memes
+            <Nav.Link
+              className="font-secondary fs-10 ms-lg-1 ms-md-3 ms-1"
+              href="/contact "
+            >
+              Contact Us
             </Nav.Link>
           </Nav>
         </Navbar.Collapse>
