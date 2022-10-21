0c5dde3cedd296e2c84f177c3febefcbfffa0a94
Borgeel <irfancesir93@gmail.com>
2022-10-21 23:50:11 +0200

    added browser router and routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,34 @@
+import { Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import DentalAesthetics from "./pages/DentalAesthetics";
+import HairTransplantation from "./pages/HairTransplantation";
+import Home from "./pages/Home";
+import MedicalAesthetics from "./pages/MedicalAesthetics";
+import SexualWellness from "./pages/SexualWellness";
+import StemCellTherapy from "./pages/StemCellTherapy";
 
 function App() {
   return (
     <>
       <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="about" element={<About />}></Route>
+        <Route
+          path="medical-aesthetics"
+          element={<MedicalAesthetics />}
+        ></Route>
+        <Route path="dental-aesthetics" element={<DentalAesthetics />}></Route>
+        <Route
+          path="hair-transplantation"
+          element={<HairTransplantation />}
+        ></Route>
+        <Route path="stem-cell-therapy" element={<StemCellTherapy />}></Route>
+        <Route path="sexual-wellness" element={<SexualWellness />}></Route>
+        <Route path="contact" element={<Contact />}></Route>
+      </Routes>
     </>
   );
 }
--- src/pages/MedicalAesthetics.js
+++ src/pages/MedicalAesthetics.js
@@ -1,5 +1,5 @@
-function Home() {
-  return <h1>Home</h1>;
+function MedicalAesthetics() {
+  return <h1>MedicalAesthetics</h1>;
 }
 
-export default Home;
+export default MedicalAesthetics;
