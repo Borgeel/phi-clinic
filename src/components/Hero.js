bcb44e131aadfce76233fcec278325aa27bd78e3
Zana Garaplija <zana.garaplija@genijalno.ba>
2022-10-22 22:20:05 +0200

    Hero styles

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,14 +1,18 @@
-import woman from "../assets/images/woman.png"
-
 const Hero = () => {
   return(
     <div className="hero-container">
-      <div>
-        <div className="hero-title">Unleash Your Inner Radiance And Natural Beauty</div>
-      </div>
-      <div className="hero-image-container">
-        <img src={woman} className="hero-image"/>
-      </div>
+      <span className="hero-text-container">
+        <span className="hero-title-container">
+          <span className="hero-title">Unleash Your Inner Radiance</span>
+        </span>
+        <span className="hero-title-container">
+          <span className="hero-title">And Natural Beauty</span>
+        </span>
+        {/* <span className="hero-title-container"> */}
+          <span className="hero-subtitle">Get the best results with us at PHI Clinic</span>
+          {/* </span> */}
+        
+      </span>
     </div>
   );
 }
