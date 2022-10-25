33e479d1739ad444ae50e78b35b76b74d6a777f4
Borgeel <irfancesir93@gmail.com>
2022-10-26 01:16:31 +0200

    added about us section to homepage

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
   return (
     <>
       <Navbar />
-      <Hero />
+      {/* <Hero /> */}
       <Routes>
         <Route path="/" element={<Home />}></Route>
         <Route
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,36 @@
-import Hero from "../components/Hero";
-import Marquee from "../components/Marquee";
+import { Button, Card, Col, Container, Row } from "react-bootstrap";
+
+import home1 from "../assets/images/home1.jpg";
 
 function Home() {
   return (
-    <>
-      <Hero />
-      <Marquee />
-    </>
+    <Container fluid="sm">
+      <Row className="mt-3">
+        <Col xs={6} className="p-3">
+          <h2>About Phi Clinic</h2>
+          <p>
+            PHI Clinic is a group of Premiere Medical and Dental Aesthetic
+            clinics located in Kuala Lumpur, Malaysia. PHI Clinic is completely
+            equipped with the most sophisticated and cuing-edge treatments in
+            aesthetic medicine administered and managed by a team
+          </p>
+          <Button variant="outline-dark m-1" size="sm">
+            Read more
+          </Button>
+          <Button variant="warning" size="sm">
+            Book A consultation
+          </Button>
+        </Col>
+        <Col xs={5} className="p-0 d-flex justify-content-center">
+          <img
+            src={home1}
+            alt="about us"
+            className="img-fluid rounded"
+            style={{ maxHeight: "70vh" }}
+          />
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
