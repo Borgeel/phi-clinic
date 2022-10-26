21e8e553a84c56eb13645abd49dc4ccab92c9556
Zana Garaplija <zanagaraplijaa@gmail.com>
2022-10-26 02:13:01 +0200

    Playing around with hero image

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,17 +1,18 @@
 const Hero = () => {
   return(
-    <div className="hero-container">
+    <div className="hero-container hero-image">
+      {/* <span className="hero-image"></span> */}
       <span className="hero-text-container">
         <span className="hero-title-container">
-          <span className="hero-title">Unleash Your Inner Radiance</span>
+        <span className="hero-title">
+            <span style={{textDecoration: "underline"}}>Unleash</span>
+            &nbsp;Your Inner Radiance
+          </span>
         </span>
         <span className="hero-title-container">
           <span className="hero-title">And Natural Beauty</span>
         </span>
-        {/* <span className="hero-title-container"> */}
           <span className="hero-subtitle">Get the best results with us at PHI Clinic</span>
-          {/* </span> */}
-        
       </span>
     </div>
   );
